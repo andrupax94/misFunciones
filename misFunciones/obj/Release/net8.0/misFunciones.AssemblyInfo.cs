56633cd72baa3673537d986da9684b49b4ebef15
andrupax94 <palenciadj94@gmail.com>
2025-03-13 14:36:51 +0000

    fghfg

--- misFunciones/obj/Release/net8.0/misFunciones.AssemblyInfo.cs
+++ misFunciones/obj/Release/net8.0/misFunciones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("misFunciones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e041ceb90914c7cbef4f21447394c2734d30549c")]
 [assembly: System.Reflection.AssemblyProductAttribute("misFunciones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("misFunciones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
